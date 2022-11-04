96c5eb817fb7250bfaa8c81e94104a18c6fc4214
Lucas Rodrigues <lucas.rodrigues@compasso.com.br>
2022-11-04 14:15:32 -0300

    Adjusting entity

--- backend/src/main/java/br/com/project/eCommerce/entities/ProductEntity.java
+++ backend/src/main/java/br/com/project/eCommerce/entities/ProductEntity.java
@@ -1,12 +1,14 @@
 package br.com.project.eCommerce.entities;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -30,4 +32,8 @@ public class ProductEntity {
 	
 	@Column(name = "image_url")
 	private String image;
+	
+	@OneToMany
+	private List<SalesEntity> salesEntity;
+	
 }
--- backend/src/main/java/br/com/project/eCommerce/entities/UserEntity.java
+++ backend/src/main/java/br/com/project/eCommerce/entities/UserEntity.java
@@ -1,9 +1,12 @@
 package br.com.project.eCommerce.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -22,5 +25,8 @@ public class UserEntity {
 	private String name;
 
 	private String password;
+	
+	@OneToMany
+	private List<SalesEntity> salesEntity;
 
 }
